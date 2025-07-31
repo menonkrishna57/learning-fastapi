629fd6cce7f1ca9d5bca97b637905166739021d4
Krishna Menon <menonkrishna57@gmail.com>
2025-07-31 13:54:00 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from urllib import response
+from fastapi import FastAPI, HTTPException, Query
+from pydantic import BaseModel
+app = FastAPI()
+
+
+class Item(BaseModel):
+    text:str
+    is_done:bool=False
+
+items=[]
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+
+@app.post("/items")
+def create_item(item: Item):
+    items.append(item)
+    return items
+
+
+@app.get("/items", response_model=list[Item])
+def list_items(limit: int=10):
+    return items[:limit]
+
+
+@app.get("/items/{item_id}",response_model=Item)
+def get_item(item_id:int)->Item:
+    if item_id < 0 or item_id >= len(items):
+        raise HTTPException(status_code=404, detail="Item not found")
+    else:
+        item= items[item_id]
+        return item
